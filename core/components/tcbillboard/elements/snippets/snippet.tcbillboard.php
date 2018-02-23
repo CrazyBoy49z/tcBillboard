0c68f3c0392fd85eea8a2cd69690be7bca8f5121
Marabar <marat@marabar.ru>
2018-02-23 11:29:13 +0500

    Добавлена пагинация pdoPage

--- core/components/tcbillboard/elements/snippets/snippet.tcbillboard.php
+++ core/components/tcbillboard/elements/snippets/snippet.tcbillboard.php
@@ -13,19 +13,23 @@ if (!$modx->loadClass('tcBillboard', MODX_CORE_PATH . 'components/tcbillboard/mo
 $tcBillboard = new tcBillboard($modx, $scriptProperties);
 
 $tpl = $modx->getOption('tpl', $scriptProperties, 'tcBillboardMyOrdersTpl', true);
+$tplWrapper = $modx->getOption('tplWrapper', $scriptProperties, 'tcbillboardMyOrdersWrapperTpl', true);
 $user = $modx->getOption('user', $scriptProperties, $modx->user->id, true);
 $sortby = $modx->getOption('sortby', $scriptProperties, 'DESC');
 $fields = $modx->getOption('fields', $scriptProperties, '', true);
+$pagination = $modx->getOption('pagination', $scriptProperties, true);
 
 $dateFormat = $tcBillboard->config['dateFormat'];
 $fields = array_map('trim', explode(',', $fields));
 
 $output = '';
+$items = $pagination == true ? array() : '';
 $tmp = array();
 
 $q = $modx->newQuery('tcBillboardOrders', array('user_id' => (int)$user));
 $q->select($modx->getSelectColumns('tcBillboardOrders', 'tcBillboardOrders', '', $fields));
 $q->sortby('id', $sortby);
+//$q->limit(5);
 
 if ($q->prepare() && $q->stmt->execute()) {
     while ($row = $q->stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -44,9 +48,27 @@ if ($q->prepare() && $q->stmt->execute()) {
             $row['end_stock'] = date($dateFormat, strtotime($row['end_stock']));
         }
 
-        $output .= $tcBillboard->getChunk($tpl, $row);
+        if ($pagination) {
+            $items[] = $tcBillboard->getChunk($tpl, $row);
+        } else {
+            $items .= $tcBillboard->getChunk($tpl, $row);
+        }
     }
 }
+
+// Пагинация pdoPage
+if ($pagination) {
+    $modx->setPlaceholder($totalVar, count($items));
+    $items = array_slice($items, $offset, $limit, true);
+
+    foreach ($items as $item) {
+        $t .= $item;
+    }
+}
+
+$output = $tcBillboard->getChunk($tplWrapper, array(
+    'output' => $pagination == true ? $t : $items,
+));
 
 if (empty($tpl)) {
     print '<pre>';
